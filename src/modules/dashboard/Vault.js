a9a5cb802874665d22a8882a7860a366415995fd
Nischit Pradhan <nischit@nischit.nischit>
2022-04-21 12:04:58 +0545

    minor vault changes

--- src/modules/dashboard/Vault.js
+++ src/modules/dashboard/Vault.js
@@ -7,6 +7,12 @@ class Vault extends React.Component {
     super(props);
     this.state = {};
     this.helper = new VaultHelper();
+
+    this.vaultAddress = "0x6b6941539Fd35dc7168bbCC4De76b3C3775b10B1";
+    // this.tokenAddress = "0x99B12715269EBfaF78C427381A60cf0ed4De8AA0";
+
+    // main net
+    this.tokenAddress="0x7EF56b89C5fe0758831ac73aA9b2368917e44148"
   }
 
   render() {
@@ -14,25 +20,31 @@ class Vault extends React.Component {
       <div>
         <hr />
         <h1>Vault</h1>
-        Vault Address: <input
+        Vault Address:{" "}
+        <input
           id={id.input.vault.DEPOSIT_VAULT_ADDRESS}
           placeholder="vault address"
-          value="0x6b6941539Fd35dc7168bbCC4De76b3C3775b10B1"
+          value={this.vaultAddress}
           disabled={true}
-        /><br/>
-        Token Address: <input
+        />
+        <br />
+        Token Address:{" "}
+        <input
           id={id.input.vault.DEPOSIT_TOKEN_ADDRESS}
           placeholder="token address"
-          value="0x99B12715269EBfaF78C427381A60cf0ed4De8AA0"
+          value={this.tokenAddress}
           disabled={true}
-        /><br/>
-        <br/>
-        <b>Config Allowance for Vault</b><br/>
+        />
+        <br />
+        <br />
+        <b>Config Allowance for Vault</b>
+        <br />
         <input id={id.input.vault.APPROVE_ALLOWANCE_AMOUNT} placeholder="allowance amount" type="number" />
         <button onClick={this.helper.approveAllowance}>Approve Allowance</button>
-        <br/>
-        <br/>
-        <b>Deposit</b><br/>
+        <br />
+        <br />
+        <b>Deposit</b>
+        <br />
         <input id={id.input.vault.DEPOSIT_TOKEN_AMOUNT} placeholder="token amount" type="number" />
         <button onClick={this.helper.deposit}>Deposit</button>
         <hr />
